d4c91a360cc3f4bf99ec8d2e885aa88d7cb98f29
Thijs Boom <twboom@hotmail.nl>
2022-09-12 17:53:28 +0200

    Small tweak

--- js/ionComponent.js
+++ js/ionComponent.js
@@ -5,7 +5,7 @@ class IonDisplay extends HTMLElement {
         // Get element
         const element = this.innerText;
         const count = this.hasAttribute('count') ? this.getAttribute('count') : '';
-        const charge = this.hasAttribute('charge') ? this.getAttribute('charge') : '+'
+        const charge = this.hasAttribute('charge') ? this.getAttribute('charge') : ''
 
         // Create the text
         const text = `${element}<sub>${count}</sub><sup>${charge}</sup>`;

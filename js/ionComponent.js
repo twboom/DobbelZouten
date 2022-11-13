53ebe2fe53bf9e5e9cd3cd913babee16b121ec2c
Thijs Boom <twboom@hotmail.nl>
2022-11-13 22:33:01 +0100

    Did things

--- js/ionComponent.js
+++ js/ionComponent.js
@@ -1,3 +1,26 @@
+class Ion {
+    constructor(elements, charge) {
+        this.elements = elements;
+        this.charge = charge;
+    };
+
+    html() {
+        // Empty string
+        let text = ''
+        console.log(this.elements)
+        // Elements
+        for (let i = 0; i < this.elements.length; i++) {
+            const el = this.elements[i];
+            text = text.concat(el[0].charAt(0).toUpperCase() + el[0].slice(1));
+            if (el[1]) { text = text.concat(`<sub>${el[1]}</sub>`) }
+        };
+        
+        // Charge
+        text = text.concat(`<sup>${this.charge}</sup>`)
+        return text
+    };
+};
+
 class IonDisplay extends HTMLElement {
     constructor() {
         super();

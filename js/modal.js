4256f09fe0e24e9e56987665ce8b773699a34f2c
Thijs Boom <twboom@hotmail.nl>
2022-11-13 21:49:36 +0100

    Made fancy modal animation

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,34 @@
+const menuExitKeyframes = [
+    { 'transform': 'translateY(0)' },
+    { 'transform': 'translateY(-110%)' },
+]
+
+const menuExitTiming = {
+    duration: 1000,
+    iterations: 1,
+    easing: 'ease',
+}
+
+const modalExitKeyframes = [
+    { 'opacity': '100%' },
+    { 'opacity': '0' },
+]
+
+const modalExitTiming = {
+    duration: 1000,
+    iterations: 1,
+    easing: 'ease',
+}
+
 function showModal() {
     document.getElementById('modal').style.display = 'block';
 };
 
 function hideModal() {
-    document.getElementById('modal').style.display = 'none';
+    document.getElementById('modal').animate(modalExitKeyframes, modalExitTiming);
+    document.getElementById('menu').animate(menuExitKeyframes, menuExitTiming).addEventListener('finish', _ => {
+        document.getElementById('modal').style.display = 'none';
+    });
 };
 
 document.getElementById('menu-show').addEventListener('click', showModal);

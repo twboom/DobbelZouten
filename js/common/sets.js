9287207c28020221b1411fa02cb0b7c1dd25f16a
Thijs Boom <twboom@hotmail.nl>
2023-01-27 18:17:53 +0100

    Made basics for localstorage implementation for ionsets

---
+++ js/common/sets.js
@@ -0,0 +1,70 @@
+const IonSets = [];
+
+IonSets.IonSet = class {
+    constructor(name, contents) {
+        this.name = name;
+        this.contents = contents;
+        this.slug = '';
+
+        for (let i = 0; i < name.lenth; i++) {
+            let char = name[i];
+            if (/^[A-Za-z0-9 ]*$/.test(char)) {
+                if (char === ' ') { char = '-' }
+                slug += char
+            } else {
+                continue
+            };
+        };
+    };
+
+    get json() {
+        return {
+            'name': this.name,
+            'slug': this.slug,
+            'contents': this.contents,
+        };
+    };
+
+    save() {
+        IonSets.set(this.slug, this.json);
+    };
+
+    update(newContents) {
+        this.contents = newContents;
+        this.save();
+    };
+};
+
+IonSets.get = function(slug) {
+    const set = JSON.parse(localStorage.getItem('ionsets'))[slug];
+    return new IonSets.IonSet(set.name, set.contents);
+};
+
+IonSets.set = function(slug, value) {
+    let sets = JSON.parse(localStorage.getItem('ionsets'));
+    sets[slug] = value;
+    Object.keys(sets).forEach(key => sets[key] === undefined ? delete sets[key] : {});
+    if (slug === undefined) { return; };
+    localStorage.setItem('ionsets', JSON.stringify(sets))
+};
+
+function init() {
+    const sets = localStorage.getItem('ionsets');
+
+    if (sets === undefined) {
+        localStorage.setItem('ionsets', '{}');
+        console.warn('LS: No IonSets found');
+        return;
+    };
+
+    try {
+        JSON.parse(sets);
+    } catch(error) {
+        localStorage.setItem('ionsets', '{}');
+        console.log('LS: Problem with the IonSets storage item, resetting.');
+    };
+};
+
+init();
+
+export default IonSets;
